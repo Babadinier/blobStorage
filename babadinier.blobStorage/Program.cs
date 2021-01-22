ceb794b9e659e1f6779faf52c0512d8427e88def
Guillaume BADINIER <gbadinier@saretec.fr>
2021-01-22 09:51:07 +0100

    Add method CreateBlobContainer

--- babadinier.blobStorage/Program.cs
+++ babadinier.blobStorage/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 
 namespace babadinier.blobStorage
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly string connectionString = "<YOUR-CONNECTION-STRING>";
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await CreateBlobContainer();
+
+            System.Console.WriteLine("Done");
+            System.Console.ReadLine();
+        }
+
+        private static async Task CreateBlobContainer()
+        {
+            // Create a BlobServiceClient object which will be used to create a container client
+            var blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            var containerName = $"container-{Guid.NewGuid().ToString()}";
+
+            // Create the container and return a container client object
+            var containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
         }
     }
 }
